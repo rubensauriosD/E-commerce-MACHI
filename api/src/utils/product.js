5b52d8afac3f3dd1576a4b5014a8669849e44d22
Mauro <mauro.arnedo7@gmail.com>
2021-10-26 17:19:23 -0300

    Endpoint searchbar

--- api/src/utils/product.js
+++ api/src/utils/product.js
@@ -1,85 +1,91 @@
-const {Producto} = require('../db')
+const { Producto, Op } = require("../db");
 
-async function getProductos(req, res) 
-{
-    let product = await Producto.findAll();
-
-    try 
-    {
-        if (product) 
-        {
-            res.json(product);
-        } 
-        else 
-        {
-            return res.status(404).send('No hay productos existentes.')
-        }
-    } 
-    catch (error) {
-        return res.status(404);
+async function getProductos(req, res) {
+  try {
+    let { nombre } = req.query;
+    let products = []
+    //#region search by NAME
+    if (nombre && nombre !== "") {
+        products = await Producto.findAll({
+            where: {
+                nombre: {
+                    [Op.iLike]: `%${nombre}%`
+                }
+            },
+        })
+    } else {
+        products = await Producto.findAll() //Si no hay input devuelve todo
     }
+    //#endregion NAME
+
+    return res.send({
+      products: products
+    });
+  } catch (error) {
+    return res.status(404);
+  }
 }
 
-async function deleteProductos(req, res) 
-{
-    const {id} = req.params;
+async function deleteProductos(req, res) {
+  const { id } = req.params;
 
-    let product = await Producto.findByPk(id)
+  let product = await Producto.findByPk(id);
 
-    Producto.destroy({
-        where:
-        {
-            id:id
-        }
-    })
+  Producto.destroy({
+    where: {
+      id: id,
+    },
+  });
 
-    res.json(product)//devuelvo el producto eliminado
+  res.json(product); //devuelvo el producto eliminado
 }
 
-async function putProductos(req, res) 
-{
-    const {id} = req.params;
+async function putProductos(req, res) {
+  const { id } = req.params;
 
-    const {nombre,imagen,precio,descripcion,disponibilidad} = req.body;
+  const { nombre, imagen, precio, descripcion, disponibilidad } = req.body;
 
-    await Producto.update(
-        {
-            nombre,imagen,precio,descripcion,disponibilidad
-        },
-        {
-            where: {id:id}
-        }
-    )
-
-    const product = await Producto.findByPk(id)
-    res.json(product)
-}
-
-async function postProductos(req, res) 
-{
-    const {nombre,imagen,precio,descripcion,disponibilidad} = req.body;
-
-    try 
+  await Producto.update(
     {
-            const nuevoProduct = await Producto.create({
-                nombre:nombre,
-                imagen:imagen,
-                precio:precio,
-                descripcion:descripcion,
-                disponibilidad: disponibilidad
-            })
-
-            res.json(nuevoProduct)
-    } 
-    catch (error) 
+      nombre,
+      imagen,
+      precio,
+      descripcion,
+      disponibilidad,
+    },
     {
-        console.log(error);
+      where: { id: id },
     }
+  );
+
+  const product = await Producto.findByPk(id);
+  res.json(product);
+}
+
+async function postProductos(req, res) {
+  const { nombre, imagen, precio, descripcion, disponibilidad } = req.body;
+  try {
+    if (!nombre || !imagen || !precio || !descripcion) {
+      // Validacion
+      res.status(404).send("The product need more data");
+    } else {
+      const nuevoProduct = await Producto.create({
+        nombre: nombre,
+        imagen: imagen,
+        precio: precio,
+        descripcion: descripcion,
+        disponibilidad: disponibilidad,
+      });
+      res.json(nuevoProduct); //Falta modificar luego de realizar las relaciones de base de datos!!
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = {
-    getProductos,
-    putProductos,
-    postProductos,
-    deleteProductos
+  getProductos,
+  putProductos,
+  postProductos,
+  deleteProductos,
 };
