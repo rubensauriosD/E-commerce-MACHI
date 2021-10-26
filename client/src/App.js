9e9fda9c1a8fba62f32950fc1579af1a497adea2
Jorge <jorgecuadradovelasquez@gmail.com>
2021-10-26 11:04:32 -0500

    componente visual provisional de navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,12 @@ import { Route } from "react-router";
 import MainPage from "./Pages/MainPage";
 import { HashRouter as Router } from "react-router-dom";
 
-import PrimarySearchAppBar from "./Components/NavBar";
+import NavBar from "./Components/NavBar";
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={PrimarySearchAppBar} />
+        <Route path="/" component={NavBar} />
         <Route exact path="/" component={MainPage} />
         <h3>Front Funcionando</h3>
       </Router>
