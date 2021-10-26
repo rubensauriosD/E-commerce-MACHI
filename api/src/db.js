28b1fae7edf5cee965fd30156fee8620906412f5
Any <anyeldayana@gmail.com>
2021-10-26 17:08:44 -0500

    relaciones

--- api/src/db.js
+++ api/src/db.js
@@ -1,31 +1,40 @@
 require('dotenv').config();
 const { Sequelize, Op } = require('sequelize');
+const {DB_USER, DB_PASSWORD, DB_HOST} = process.env;
+
 const UsuarioModel = require('./models/usuarios');
 const ProductosModel = require('./models/productos');
 const ImagenesModel = require('./models/imagenes');
 const ComentariosModel = require('./models/comentarios');
 
-const {
-    DB_USER, DB_PASSWORD, DB_HOST,
-  } = process.env;
-  
-  const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/ecommerce_machi`, {
-    logging: false, 
-    native: false, 
-  });
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/ecommerce_machi`, {
+  logging: false,
+  native: false,
+});
 
-  const Usuario = UsuarioModel(sequelize)
-  const Producto = ProductosModel(sequelize)
-  const Imagenes = ImagenesModel(sequelize)
-  const Comentarios = ComentariosModel(sequelize)
+const Usuario = UsuarioModel(sequelize)
+const Producto = ProductosModel(sequelize)
+const Imagenes = ImagenesModel(sequelize)
+const Comentario = ComentariosModel(sequelize)
 
-  // Relaciones
+// *Relaciones*
+//Usuario(admin) - Producto:
+Usuario.hasMany(Producto);
+Producto.belongsTo(Usuario);
 
-  module.exports = {
-      conn: sequelize,
-      Usuario,
-      Producto,
-      Imagenes,
-      Comentarios,
-      Op
-  };
+//comentario - Producto - Usuario:
+Producto.hasMany(Comentario);
+Usuario.hasMany(Comentario);
+Comentario.belongsTo(Usuario);
+Comentario.belongsTo(Producto);
+
+
+module.exports = {
+  conn: sequelize,
+  Usuario,
+  Producto,
+  Imagenes,
+  Comentarios,
+  Op
+};
+
