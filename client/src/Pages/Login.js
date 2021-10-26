121e173a481972c6a9f9b4a601e4b639872ac67e
akiibajin <julianrobayo8000@gmail.com>
2021-10-26 16:28:30 -0500

    Añadir Login,SignIn en Front

---
+++ client/src/Pages/Login.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {postUser,getUsers} from "../Redux/actions/action"
+import  {useDispatch,useSelector} from "react-redux"
+import Login from "../Components/LogIn";
+import SignIn from "../Components/SignIn";
+const LoginPage = () => {
+  const dispatch=useDispatch()
+  const IniciarSesion=(Usuario)=>{
+    dispatch(getUsers(Usuario))
+  }
+  const Registrarse=(Usuario)=>{
+    dispatch(postUser(Usuario))
+  }
+  return (
+    <div>
+      <Login SubmitL={IniciarSesion}/>
+      <SignIn SubmitS={Registrarse}/>
+    </div>
+  );
+};
+
+export default LoginPage;
