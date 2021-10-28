7a948c33e8cc11b9f016533e5a603467f11f4752
Jorge <jorgecuadradovelasquez@gmail.com>
2021-10-27 19:48:51 -0500

    le instale una dependencia que le faltaba

--- client/src/Components/Categoria.jsx
+++ client/src/Components/Categoria.jsx
@@ -1,14 +1,13 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
-export default function Categoria({nombreCategoria,imagenCategoria}){
-
-    return(
-        <Link to={`/tienda/${nombreCategoria}`}>
-        <div styles={`background-image:url(${imagenCategoria}); background-size:30vw 20vh`}>
-        {/* <div> */}
-            <p>{nombreCategoria}</p>
-            {/* <img src={imagenCategoria}alt="sin imagen"/> */}
-        </div>
-        </Link>
-    )
+export default function Categoria({ nombreCategoria, imagenCategoria }) {
+  return (
+    <Link to={`/tienda/${nombreCategoria}`}>
+      {/* <div styles={`background-image:url(${imagenCategoria}); background-size:30vw 20vh`}> */}
+      <div>
+        <p>{nombreCategoria}</p>
+        <img src={imagenCategoria} alt="sin imagen" />
+      </div>
+    </Link>
+  );
 }
