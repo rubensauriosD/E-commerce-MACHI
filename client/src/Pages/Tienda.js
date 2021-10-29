3c0dbe182d8126ab4325a37956dd92bdac722bd9
Mauro <mauro.arnedo7@gmail.com>
2021-10-29 11:22:58 -0300

    cambios y filtros para la tienda parte 2

--- client/src/Pages/Tienda.js
+++ client/src/Pages/Tienda.js
@@ -1,17 +1,43 @@
 import {useSelector,useDispatch} from "react-redux"
 import * as React from "react"
-import {productosFiltrados} from "../Redux/actions/action"
+// import {productosFiltrados} from "../Redux/actions/action"
 import Products from "../Components/Products"
-export default function Tienda({props}){
-    const productosPorCategorias=useSelector(state=>state.productosPorCategorias)
+import SearchBar from "../Components/SearchBar"
+import FiltrosYOrden from "../Components/FiltrosYOrden"
+import { getProducts, setPagina } from "../Redux/actions/action"
+
+export default function Tienda({nombreCategoria}){
+    // const productosPorCategorias=useSelector(state=>state.productosPorCategorias)
+    const { products, nombre, ordenA, ordenP, filtroC, pagina } = useSelector(state => state)
     const dispatch=useDispatch()
     React.useEffect(()=>{
-        dispatch(productosFiltrados(props))
-    },[dispatch])
+        // dispatch(productosFiltrados(props))
+        dispatch(getProducts({filtroC: nombreCategoria ? nombreCategoria : "" }))
+    },[dispatch, nombreCategoria])
+
+    const changePagina = (pagina) => {
+        dispatch(getProducts({ pagina, ordenA, ordenP, filtroC, nombre }))
+        dispatch(setPagina(pagina))
+    }
+
     return(
         <div>
-            <h1>{props.nombreCategoria}</h1>
-            <Products productos={productosPorCategorias}/> 
+            <div>
+                <SearchBar />
+            </div>
+            <div>
+                <FiltrosYOrden />
+            </div>
+            {/* <h1>{props.nombreCategoria}</h1> */}
+            {/* <Products productos={productosPorCategorias}/>  */}
+            <div>
+                <Products productos={products.resultado} />
+            </div>
+            <div>
+            <button disabled={pagina - 1 === 0} onClick={() => { changePagina(pagina - 1) }}>Anterior</button>
+                <label>{pagina}</label>
+            <button disabled={products?.contador <= (pagina * 6)} onClick={() => { changePagina(pagina + 1) }}>Siguiente</button>
+            </div>
         </div>
     )
 }
