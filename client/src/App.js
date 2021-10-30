999cca89e2522bc82721fe2cac4d09af82e51d56
Jorge <jorgecuadradovelasquez@gmail.com>
2021-10-29 22:49:15 -0500

    funcionando lo de contacto

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { Route, Switch } from "react-router";
 import MainPage from "./Pages/MainPage";
 import { HashRouter as Router } from "react-router-dom";
-import Contacto from "./Pages/Contacto";
+import ContactPage from "./Pages/ContactPage";
 import Login from "./Pages/Login";
 import Tienda from "./Pages/Tienda";
 import NavBar from "./Components/NavBar";
@@ -14,11 +14,23 @@ function App() {
         <Route path="/" component={NavBar} />
         <Switch>
           <Route exact path="/" component={MainPage} />
-          <Route exact path="/contact" component={Contacto} />
+          <Route exact path="/contact" component={ContactPage} />
           <Route exact path="/login" component={Login} />
-          <Route exact path="/tienda/:nombreCategoria" render={({match})=><Tienda props={match.params.nombreCategoria}/>}/>
-          <Route exact path="/producto/:idProducto" render={({match})=><DetalleProducto props={match.params.idProducto}/>}/>
-        </Switch> 
+          <Route
+            exact
+            path="/tienda/:nombreCategoria"
+            render={({ match }) => (
+              <Tienda props={match.params.nombreCategoria} />
+            )}
+          />
+          <Route
+            exact
+            path="/producto/:idProducto"
+            render={({ match }) => (
+              <DetalleProducto props={match.params.idProducto} />
+            )}
+          />
+        </Switch>
       </Router>
     </div>
   );
