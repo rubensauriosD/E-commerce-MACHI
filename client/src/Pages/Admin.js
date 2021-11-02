5c9d19e4b04073d22d7d3e00b42ee308e938f6bd
verita022 <verita022@gmail.com>
2021-11-01 21:57:37 -0300

    estilo basico Admin

--- client/src/Pages/Admin.js
+++ client/src/Pages/Admin.js
@@ -4,6 +4,7 @@ import {useDispatch, useSelector} from 'react-redux';
 import { useHistory } from "react-router-dom";
 import {getProductsAdmin, deleteProduct, putProduct, getImages, deleteImage, postImage, postProduct} from '../Redux/actions/action';
 import axios from 'axios'
+import '../Styles/AdminStyle.css'
 
 
 
@@ -67,106 +68,141 @@ const Admin = () => {
     }
 
     return (
-        <div>
+        <div className="adminContenedor">
             <h1>Admin</h1>
+            <div className='formsAdminExterno'>
         
+            <div className='formsAdmin'>
             <form >
-                <h3>Producto</h3>
-                <label>Nombre</label>
-                <input type='text' name='nombre' onChange={(e) => handleChange(e)} required/>
+                <h3>Cargar Producto </h3>
+                <div class="levelAdmin">
+                    <label>Nombre: </label>
+                    <input type='text' name='nombre' onChange={(e) => handleChange(e)} required/>
+                </div>
                 
-                <label>Precio</label>
-                <input type='number' name='precio' onChange={(e) => handleChange(e)} required/>
+                <div class="levelAdmin">
+                    <label>Precio: </label>
+                    <input type='number' name='precio' onChange={(e) => handleChange(e)} required/>
+                </div>
 
-                <label>Descripcion</label>
-                <input type='text' name='descripcion' onChange={(e) => handleChange(e)} />
+                <div class="levelAdmin">   
+                    <label>Descripcion: </label>
+                    <input type='text' name='descripcion' onChange={(e) => handleChange(e)} />
+                </div >
 
-                <label>Imagen</label>
-                <input type='file' name='imagen' onChange={(e) => handleChange(e)}/>
+                <div class="levelAdmin">
+                    <label>Disponibilidad: </label>
+                    <select name='disponibilidad' onChange={(e) => handleChange(e)} required>
+                    <option value='true'>Disponible</option>     
+                    <option value='false'>No Disponible</option>
+                    </select> 
+                </div>                   
 
-                <label>Disponibilidad</label>
-                <select name='disponibilidad' onChange={(e) => handleChange(e)} required>
-                <option value='true'>Disponible</option>     
-                <option value='false'>No Disponible</option>
-                </select>                    
+                <div class="levelAdmin">
+                    <label>Categoria: </label>
+                    <select name='categoria' onChange={(e) => handleChange(e)} required>
+                        <option value="Cajones y Cultivos">Cajones y Cultivos</option>
+                        <option value="Plantines y Semillas">Plantines y Semillas</option>
+                        <option value="Composteras">Composteras</option>
+                        <option value="Insumos y Herramientas para Huertas">Insumos y Herramientas para Huertas</option>
+                        <option value="Lombrices Rojas Californeanas">Lombrices Rojas Californeanas</option>
+                    </select>
+                </div>
 
-                <label>Categoria</label>
-                <select name='categoria' onChange={(e) => handleChange(e)} required>
-                    <option value="Cajones y Cultivos">Cajones y Cultivos</option>
-                    <option value="Plantines y Semillas">Plantines y Semillas</option>
-                    <option value="Composteras">Composteras</option>
-                    <option value="Insumos y Herramientas para Huertas">Insumos y Herramientas para Huertas</option>
-                    <option value="Lombrices Rojas Californeanas">Lombrices Rojas Californeanas</option>
-                </select>
+                <div class="levelAdmin">
+                    <label>Seleccione la imagen: </label>
+                    <input type='file' name='imagen' onChange={(e) => handleChange(e)}/>
+                </div>
 
-                <button type='submit' onClick={()=> dispatch(postProduct(inputs))}>Crear</button>
+                <button className="buttonAdmin" type='submit' onClick={()=> dispatch(postProduct(inputs))}>Crear</button>
             </form>
-
+            </div>
+            <hr/> 
             <form>
-                <h3>Imagen</h3>
+                <h3>Imagen Galeria</h3>
 
-                <label>Seleccione la imagen</label>
+                <label>Seleccione la imagen: </label>
                 <input type='file' name='imagen' onChange={(e)=>{
                     handleChangeImagen(e)
                 }} required/>
 
-                <button type='submit' onClick={() => dispatch(postImage(img))}>Crear</button>
+                <br/><button className="buttonAdmin" type='submit' onClick={() => dispatch(postImage(img))}>Crear</button>
             </form>
+            
+            <hr/>
 
             <h3>Editar Productos</h3>
             <ol>
                 {
-                     productos.productos?.map((producto,i) => {
+                     productos.productos?.map((producto, i) => {
                         return(
                             
-                            <li key={producto.id}>
-                                
-                                <img src={producto.imagen} alt="imagen producto"/>
+                        <li key={producto.id}>
+                        <div className="ordererAdmin">        
+                            <div className="imagenAdminDiv">    
+                                <img className="imagenAdmin" src={producto.imagen} alt="imagen producto"/>
                                 <input type='file' name='imagen' onChange={(e) => handleChangeEditar(e)}/>
+                            </div>    
+                            <div className="datosProductoAdmin">
+                                <div className="labelAdminEdit">
+                                    <label>Nombre: </label>
+                                    <input type='text' name='nombre' onChange={(e) => handleChangeEditar(e)} placeholder={producto.nombre}/><br/>
+                                </div>
 
-                                <input type='text' name='nombre' onChange={(e) => handleChangeEditar(e)} placeholder={producto.nombre}/>
+                                <div className="labelAdminEdit">
+                                    <label>Precio: </label>
+                                    <input type='number' name='precio' placeholder={producto.precio} onChange={(e) => handleChangeEditar(e)}/><br/>
+                                </div>
 
-                                <input type='number' name='precio' placeholder={producto.precio} onChange={(e) => handleChangeEditar(e)}/>
+                                <div className="labelAdminEdit">
+                                    <label>Descripcion: </label>
+                                    <input style={{height: "60px" }} type='text' name='Descripcion' placeholder={producto.descripcion} onChange={(e) => handleChangeEditar(e)}/><br/>
+                                </div>
 
-                                <input type='text' name='Descripcion' placeholder={producto.descripcion} onChange={(e) => handleChangeEditar(e)}/>
-                                
-                                <label>Disponibilidad Actual: {producto.disponibilidad? 'Disponible' : 'No disponible'}</label>
-                                <select name='disponibilidad' onChange={(e) => handleChangeEditar(e)} >   
-                                    <option value='true'>Disponible</option>     
-                                    <option value='false'>No Disponible</option>
-                                </select>
+                                <div className="labelAdminEdit">
+                                    <label>Disponibilidad Actual: {producto.disponibilidad? 'Disponible' : 'No disponible'}</label><br/>
+                                    <select name='disponibilidad' onChange={(e) => handleChangeEditar(e)} >   
+                                        <option value='true'>Disponible</option>     
+                                        <option value='false'>No Disponible</option>
+                                    </select><br/>
+                                </div>
 
-                                <label>Categoria Actual: {producto.categoria}</label>
-                                <select name='categoria'  onChange={(e) => handleChangeEditar(e)}>
-                                <option value="Cajones y Cultivos">Cajones y Cultivos</option>
-                                <option value="Plantines y Semillas">Plantines y Semillas</option>
-                                <option value="Composteras">Composteras</option>
-                                <option value="Insumos y Herramientas para Huertas">Insumos y Herramientas para Huertas</option>
-                                <option value="Lombrices Rojas Californeanas">Lombrices Rojas Californeanas</option>
-                                </select>
+                                <div className="labelAdminEdit">
+                                    <label>Categoria Actual: {producto.categoria}</label><br/>
+                                    <select name='categoria'  onChange={(e) => handleChangeEditar(e)}>
+                                    <option value="Cajones y Cultivos">Cajones y Cultivos</option>
+                                    <option value="Plantines y Semillas">Plantines y Semillas</option>
+                                    <option value="Composteras">Composteras</option>
+                                    <option value="Insumos y Herramientas para Huertas">Insumos y Herramientas para Huertas</option>
+                                    <option value="Lombrices Rojas Californeanas">Lombrices Rojas Californeanas</option>
+                                    </select><br/>
+                                </div>
 
-                                
-                                <button id={producto.id} name={producto.nombre} onClick={(e) => {
-                                        inputsEditar.nombre ? inputsEditar.nombre = inputsEditar.nombre :  inputsEditar.nombre = producto.nombre;
-                                        inputsEditar.precio ? inputsEditar.precio = inputsEditar.precio :  inputsEditar.precio = producto.precio;
-                                        inputsEditar.imagen ? inputsEditar.imagen = inputsEditar.imagen :  inputsEditar.imagen = producto.imagen;
-                                        inputsEditar.precio ? inputsEditar.precio = inputsEditar.precio :  inputsEditar.precio = producto.precio;
-                                        inputsEditar.disponibilidad ? inputsEditar.disponibilidad = inputsEditar.disponibilidad :  inputsEditar.disponibilidad = producto.disponibilidad;
-                                        inputsEditar.categoria ? inputsEditar.categoria = inputsEditar.categoria :  inputsEditar.categoria = producto.categoria;
-                                        inputsEditar.descripcion ? inputsEditar.descripcion = inputsEditar.descripcion :  inputsEditar.descripcion = producto.descripcion;
-                                        editar(e)
-                                    }
-                                }>Editar</button>
+                                <div className="labelAdminEdit">
+                                    <button className="buttonAdmin" id={producto.id} name={producto.nombre} onClick={(e) => {
+                                            inputsEditar.nombre ? inputsEditar.nombre = inputsEditar.nombre :  inputsEditar.nombre = producto.nombre;
+                                            inputsEditar.precio ? inputsEditar.precio = inputsEditar.precio :  inputsEditar.precio = producto.precio;
+                                            inputsEditar.imagen ? inputsEditar.imagen = inputsEditar.imagen :  inputsEditar.imagen = producto.imagen;
+                                            inputsEditar.precio ? inputsEditar.precio = inputsEditar.precio :  inputsEditar.precio = producto.precio;
+                                            inputsEditar.disponibilidad ? inputsEditar.disponibilidad = inputsEditar.disponibilidad :  inputsEditar.disponibilidad = producto.disponibilidad;
+                                            inputsEditar.categoria ? inputsEditar.categoria = inputsEditar.categoria :  inputsEditar.categoria = producto.categoria;
+                                            inputsEditar.descripcion ? inputsEditar.descripcion = inputsEditar.descripcion :  inputsEditar.descripcion = producto.descripcion;
+                                            editar(e)
+                                        }
+                                    }>Editar</button>
 
-                                <button id={producto.id} onClick={(e) => {
-                                    // dispatch(deleteProduct(e.target.id));
-                                    borrar(e)
-                                }}>Eliminar</button>
-                            </li>)
+                                    <button className="buttonAdmin" id={producto.id} onClick={(e) => {
+                                        // dispatch(deleteProduct(e.target.id));
+                                        borrar(e)
+                                    }}>Eliminar</button>
+                                </div>
+                            </div>  
+                        </div>          
+                        </li>)
                     }) 
                 }   
             </ol>
-
+            <hr/>    
             <h3>Editar Imagenes</h3>
             <ol>
                 {
@@ -184,6 +220,7 @@ const Admin = () => {
                     }) */
                 }
             </ol>
+            </div>
         </div>
     );
 };
