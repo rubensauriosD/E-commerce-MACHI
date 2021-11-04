e1813c6d3ff7020a5581af2214756a92c7146d7e
verita022 <verita022@gmail.com>
2021-11-03 22:04:50 -0300

    Admin-estilo

--- client/src/Components/Admin/CargarProducto.jsx
+++ client/src/Components/Admin/CargarProducto.jsx
@@ -26,50 +26,54 @@ export default function CargarProducto(){
 
 
     return ( 
-        <form >
-            <h3>Cargar Producto </h3>
-                <div class="levelAdmin">
-                    <label>Nombre: </label>
-                    <input type='text' name='nombre' onChange={(e) => handleChange(e)} required/>
-                </div>
-                        
-                <div class="levelAdmin">
-                    <label>Precio: </label>
-                    <input type='number' name='precio' onChange={(e) => handleChange(e)} required/>
-                </div>
+        <div className="productoAdmin">
+                
+                <div className="formsAdmin" >
+                <div className="formNombre" ><h2>Cargar Producto </h2></div>
+                    <form >
+                        <div className="levelAdmin">
+                            <label>Nombre </label>
+                            <input type='text' name='nombre' onChange={(e) => handleChange(e)} required/>
+                        </div>
+                                
+                        <div className="levelAdmin">
+                            <label>Precio </label>
+                            <input type='number' name='precio' onChange={(e) => handleChange(e)} required/>
+                        </div>
 
-                <div class="levelAdmin">   
-                    <label>Descripcion: </label>
-                    <input type='text' name='descripcion' onChange={(e) => handleChange(e)} />
-                </div >
+                        <div className="levelAdmin" >   
+                            <label>Descripcion </label>
+                            <input style={{height: "60px" }} type='text' name='descripcion' onChange={(e) => handleChange(e)} />
+                        </div >
 
-                <div class="levelAdmin">
-                    <label>Disponibilidad: </label>
-                    <select name='disponibilidad' onChange={(e) => handleChange(e)} required>
-                    <option value='true'>Disponible</option>     
-                    <option value='false'>No Disponible</option>
-                    </select> 
-                </div>                   
+                        <div className="levelAdmin">
+                            <label>Disponibilidad </label>
+                            <select name='disponibilidad' onChange={(e) => handleChange(e)} required>
+                            <option value='true'>Disponible</option>     
+                            <option value='false'>No Disponible</option>
+                            </select> 
+                        </div>                   
 
-                <div class="levelAdmin">
-                    <label>Categoria: </label>
-                    <select name='categoria' onChange={(e) => handleChange(e)} required>
-                        <option value="Cajones y Cultivos">Cajones y Cultivos</option>
-                        <option value="Plantines y Semillas">Plantines y Semillas</option>
-                        <option value="Composteras">Composteras</option>
-                        <option value="Insumos y Herramientas para Huertas">Insumos y Herramientas para Huertas</option>
-                        <option value="Lombrices Rojas Californeanas">Lombrices Rojas Californeanas</option>
-                    </select>
-                </div>
+                        <div className="levelAdmin">
+                            <label>Categoria </label>
+                            <select name='categoria' onChange={(e) => handleChange(e)} required>
+                                <option value="Cajones y Cultivos">Cajones y Cultivos</option>
+                                <option value="Plantines y Semillas">Plantines y Semillas</option>
+                                <option value="Composteras">Composteras</option>
+                                <option value="Insumos y Herramientas para Huertas">Insumos y Herramientas para Huertas</option>
+                                <option value="Lombrices Rojas Californeanas">Lombrices Rojas Californeanas</option>
+                            </select>
+                        </div>
 
-                <div class="levelAdmin">
-                    <label>Seleccione la imagen: </label>
-                    <input type='file' name='imagen' onChange={(e) => handleChange(e)}/>
-                </div>
-
+                        <div className="levelAdmin">
+                            <label>Seleccione la imagen </label>
+                            <input type='file' name='imagen' onChange={(e) => handleChange(e)}/>
+                        </div>
+                    </form> 
+                    
+                </div>            
                 <button className="buttonAdmin" type='submit' onClick={()=> dispatch(postProduct(inputs))}>Crear</button>
-        </form> 
-    
+        </div>
     )
 
         
