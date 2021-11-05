2aa174016c963e55f02bae4df348adac61233b8a
Jorge <jorgecuadradovelasquez@gmail.com>
2021-11-04 22:14:58 -0500

    solo removiendo los warnings

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,31 +1,34 @@
-import React, { useEffect } from "react";
-import {postUser,IniciarSesion} from "../Redux/actions/action"
-import  {useDispatch,useSelector} from "react-redux"
+import { postUser, IniciarSesion } from "../Redux/actions/action";
+import { useDispatch, useSelector } from "react-redux";
 import Login from "../Components/LogIn";
 import SignIn from "../Components/SignIn";
-import { useHistory } from "react-router-dom"
+import { useHistory } from "react-router-dom";
 import InterfazDeUsuario from "../Components/InterfazDeUsuario";
-import "../Styles/paginaDeLogin.css"
+import "../Styles/paginaDeLogin.css";
 const LoginPage = () => {
-  const history=useHistory ()
-  const usuario=useSelector(state=>state.User)
-  const dispatch=useDispatch()
+  const history = useHistory();
+  const usuario = useSelector((state) => state.User);
+  const dispatch = useDispatch();
 
-  function InicioSesion(Usuario){
-    dispatch(IniciarSesion(Usuario,history)) 
+  function InicioSesion(Usuario) {
+    dispatch(IniciarSesion(Usuario, history));
   }
 
-
-  const Registrarse=(Usuario)=>{
-    dispatch(postUser(Usuario))  
-  }
-  if(usuario&&Object.values(usuario).length!==0){     
-    return (<div className="InterfazDeUsuario"> <InterfazDeUsuario datosUsuario={usuario}/></div>)
-  }else{
+  const Registrarse = (Usuario) => {
+    dispatch(postUser(Usuario));
+  };
+  if (usuario && Object.values(usuario).length !== 0) {
+    return (
+      <div className="InterfazDeUsuario">
+        {" "}
+        <InterfazDeUsuario datosUsuario={usuario} />
+      </div>
+    );
+  } else {
     return (
       <div className="InterfazDeFormulario">
-        <Login SubmitL={InicioSesion}/>
-        <SignIn SubmitS={Registrarse}/>
+        <Login SubmitL={InicioSesion} />
+        <SignIn SubmitS={Registrarse} />
       </div>
     );
   }
