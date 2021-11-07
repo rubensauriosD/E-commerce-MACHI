cd8f3d5ecb80981cf1852d1fd539263f5631256a
Veronica Camisassa <verita022@gmail.com>
2021-11-06 21:09:30 -0300

    Update setHeaders.js

--- api/src/utils/middlewares/setHeaders.js
+++ api/src/utils/middlewares/setHeaders.js
@@ -1,5 +1,5 @@
 function setHeaders(req, res, next){
-  res.header('Access-Control-Allow-Origin', 'https://e-commerce-machi.vercel.app/#/'); 
+  res.header('Access-Control-Allow-Origin', '*'); 
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
