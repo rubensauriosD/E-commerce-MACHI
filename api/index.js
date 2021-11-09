36aa8803d7c21128017b12504082273078e6dba8
Veronica Camisassa <verita022@gmail.com>
2021-11-09 09:54:49 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const morgan = require("morgan");
 const routes = require("./src/routes");
 const errorHandler = require("./src/utils/middlewares/errorHandler");
-//const setHeaders = require("./src/utils/middlewares/setHeaders");
+const setHeaders = require("./src/utils/middlewares/setHeaders");
 const { conn } = require("./src/db");
 const passport = require("passport");
 const session = require("express-session");
-const cors = require("cors")
+const cors = require('cors')
 const app = express();
 //Inicializaciones
 require("./src/passport/autorizacion-local");
@@ -15,11 +15,11 @@ require("./src/passport/autorizacion-facebook")
 
 app.set("puerto", process.env.PORT || 3001);//puerto que pide heroku para el deploy
 
-app.use(cors())
+app.use(cors());
+app.use(setHeaders);
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json({ limit: "50mb" }));
 app.use(morgan("dev"));
-//app.use(setHeaders);
 app.use(
     session({
       secret: "eMachiComercioWebPage",
