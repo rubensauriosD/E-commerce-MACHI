235ce3fd4373ce69968472e3e8b0f9694057c7ef
Veronica Camisassa <verita022@gmail.com>
2021-11-09 09:52:34 -0300

    Update setHeaders.js

--- api/src/utils/middlewares/setHeaders.js
+++ api/src/utils/middlewares/setHeaders.js
@@ -1,5 +1,5 @@
 function setHeaders(req, res, next){
-  res.header('Access-Control-Allow-Origin', `${process.env.FRONT_HEADER || "http://localhost:3000"}`); 
+  res.header('Access-Control-Allow-Origin', '*'); 
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
