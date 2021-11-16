5ca183ac3fdeef40edb332883e68ef75060a10c2
Ruben <83410864+rubensauriosD@users.noreply.github.com>
2021-11-16 14:10:49 -0300

    Update setHeaders.js

--- api/src/utils/middlewares/setHeaders.js
+++ api/src/utils/middlewares/setHeaders.js
@@ -2,7 +2,7 @@ function setHeaders(req, res, next){
   //res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Origin', 'https://6193cf628aa05f200450a3e9--laughing-fermi-226ba3.netlify.app');
   res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,Cookie,Set-Cookie');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 };
