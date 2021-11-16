f88dda8e6b282cb58d9dc7d1910b6129cc194276
Ruben <83410864+rubensauriosD@users.noreply.github.com>
2021-11-16 14:05:14 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.use(
     secret: "eMachiComercioWebPage",
     resave: false,
     saveUninitialized: false,
-    cookie:{sameSite:true,secure:true}
+    cookie:{sameSite:none,secure:true}
   })
 );
 app.use(passport.initialize());
