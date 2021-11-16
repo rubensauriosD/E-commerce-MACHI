e9eb3422663bbfc6780e6e0831799ee362e5cb8e
Ruben <83410864+rubensauriosD@users.noreply.github.com>
2021-11-16 16:50:49 -0300

    Update facturaAction.jsx

--- client/src/Redux/actions/facturaAction.jsx
+++ client/src/Redux/actions/facturaAction.jsx
@@ -61,7 +61,7 @@ export const postFactura = (payer, items) => {
     const arregloDeIdsProductos=items?.map(item=>item.idProductos)
     return (dispatch) => {
         console.log("datos factura ruben",{nombrecompleto,telefono,direccion,arregloDeIdsProductos,ammount,total, cantidadporProducto})
-        axios.post(`/factura`, {nombrecompleto,telefono,direccion,arregloDeIdsProductos,ammount,total}, {withCredentials:true})
+        axios.post(`/factura`, {nombrecompleto,telefono,direccion,arregloDeIdsProductos,ammount,total,cantidadporProducto}, {withCredentials:true})
         .then(()=>  {
             console.log('Los datos de factura fueron enviados al back: Ammount: ', ammount, '/ Total: ', total)
             dispatch({
