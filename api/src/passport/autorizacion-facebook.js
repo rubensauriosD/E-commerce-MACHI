a9b6917fc3072d517a843e85a43be39fbef8983d
Ruben <83410864+rubensauriosD@users.noreply.github.com>
2021-11-19 00:07:11 -0300

    Update autorizacion-facebook.js

--- api/src/passport/autorizacion-facebook.js
+++ api/src/passport/autorizacion-facebook.js
@@ -4,9 +4,9 @@ const { Usuario } = require("../db");
 passport.use(
   new FacebookStrategy(
     {
-      clientID: process.env.ID_FACEBOOK || "593541735027050",
-      clientSecret: process.env.SECRET_KEY_FACEBOOK ||"f60d89eff6085886da441c385c43f8de",
-      callbackURL: process.env.CALLBACK_URL || "http://localhost:3001/usuarios/auth/facebook/inicioDeSesion",
+      clientID: process.env.ID_FACEBOOK,
+      clientSecret: process.env.SECRET_KEY_FACEBOOK,
+      callbackURL: process.env.CALLBACK_URL,
       profileFields:["id","name","photos","email","displayName"]
     },
     async function (accessToken, refreshToken, profile, cb) {
